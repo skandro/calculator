47a6dae2b981d62811e89defa983a189ace81e24
Skandro <skandro_mirsad@yahoo.com>
2022-11-07 22:14:48 +0000

    Test commit3

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner newObj = new Scanner(System.in);
-        System.out.println("--------------------------");
+        System.out.println("---------------------------");
 
-        System.out.println("Please enter value for X: ");
+        System.out.println("Please enter value for X:  ");
         float x = newObj.nextFloat();
 
-        System.out.println("Please enter value for Y: ");
+        System.out.println("Please enter value for Y:  ");
         float y = newObj.nextFloat();
 
         System.out.println("Result of sum: " + (x+y));
